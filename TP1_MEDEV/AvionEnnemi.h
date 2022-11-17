dd19765c8c190c5b3e46bac2c1eee6f453b6d064
DavidFraux <d.fraux@laposte.net>
2022-11-17 11:28:53 +0100

    commit ae.h

--- TP1_MEDEV/AvionEnnemi.h
+++ TP1_MEDEV/AvionEnnemi.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "avion.h"
-class carte
+#include "Carte.h"
 
 class AvionEnnemi :Avion
 {
 public:
 	AvionEnnemi(int x, int y, int z, int xO, int yO, int zO) :Avion(x, y, z, xO, yO, zo) {};
-	int getTeam() { return 2 };
-	void deplacement(carte);
+	int getTeam() { return 2; };
+	void decisionDeplacement(const Carte& c, int[3]);
+	void appliquerDeplacement(int[3]);
 private:
 };
