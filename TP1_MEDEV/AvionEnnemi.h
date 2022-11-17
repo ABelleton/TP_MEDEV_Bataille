82ab9e6ec5244b94bc098df75506e1868e85260c
DavidFraux <d.fraux@laposte.net>
2022-11-17 10:08:12 +0100

    Please work

---
+++ TP1_MEDEV/AvionEnnemi.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "avion.h"
+
+	class AvionEnnemi :Avion
+{
+public:
+	AvionEnnemi(int x, int y, int z, int xO, int yO, int zO) :Avion(x, y, z, xO, yO, zo){];
+	int getTeam() { return 2 };
+
+private:
+};
