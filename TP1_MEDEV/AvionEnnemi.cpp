c965d4ae5c752620eca6b1182c4d522694ec2adc
DavidFraux <d.fraux@laposte.net>
2022-11-17 11:30:04 +0100

    commit ae.cpp

--- TP1_MEDEV/AvionEnnemi.cpp
+++ TP1_MEDEV/AvionEnnemi.cpp
@@ -1,10 +1,32 @@
 #include "AvionEnnemi.h" 
 
-void AvionEnnemi::deplacement(carte){
+void AvionEnnemi::decisionDeplacement(const Carte& c, int[3] &o){
+	int[3] positionV;
+	double distV=10000.0;
+	double distA = 0.0;
+	for (int i = 0; i < 4; i++) {
+		if (c.avions[i]->getTeam() != this->getTeam()) {
+			distA = sqrt((this->getX() - c.avions[i]->getX()) ^ 2 + (this->getY() - c.avions[i]->getX()) ^ 2 + (this->getZ() - c.avions[i]->getX()) ^ 2);
+			if (distA < distV) {
+				positionV[0] = c.avions[i]->getX();
+				positionV[1] = c.avions[i]->getY();
+				positionV[2] = c.avions[i]->getZ();
+				distV = distA;
+			}
+		}
+	}
+	positionV-={this->getX(),this->getY(),this->getZ()}
+}
 
-
-
-
-
-
+void AvionEnnemi::appliquerDeplacement(int[3] o) {
+	orientation = o;
+	coordonees += o;
+	for (int i = o; i < 3; i++) {
+		if (coordonees[i] > 13) {
+			coordonees[i] = 0;
+		}
+		if (coordonees[i] < 0) {
+			coordonees[i] = 13;
+		}
+	}
 }
