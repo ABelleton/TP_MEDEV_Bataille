fccbef012134362605316cab10c3275c74145ab0
Majid Hamza <107484171+Norman-lib@users.noreply.github.com>
2022-11-17 10:17:57 +0100

    .h implemented

---
+++ TP1_MEDEV/Avion.cpp
@@ -0,0 +1,22 @@
+#include "Avion.h"
+
+Avion :: Avion(int x, int y, int z, int xo , int yo, int zo) {
+   
+    coordonees[0] = x;
+    coordonees[1] = y;
+    coordonees[2] = z;
+
+    orientation[0] = xo;
+    orientation[1] = yo;
+    orientation[2] = zo;
+
+    isDead = false;
+    cooldown = false;
+
+}
+
+
+void Avion::tir (Carte carte){
+    
+
+};
